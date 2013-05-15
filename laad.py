f75aa4c26f416fdab75572465dc87c16e9dc697d
Juan Gallisa <j.s.gallisa@gmail.com>
2013-05-14 20:27:00 -0400

    added some querying

--- laad.py
+++ laad.py
@@ -3,11 +3,11 @@ import sys
 import getpass
 
 def Gdataentry(sequences):
-"""
+    """
     print "Give desired username and password"
     user=raw_input("User:")
     password = getpass.getpass()
-"""
+    """
     try:
         #Connecting
         con = mdb.connect('localhost', 'guest', 'password', 'genomedb')
@@ -24,21 +24,32 @@ def Gdataentry(sequences):
             con.close()
 
     
-def Gquery():
-   """ print "Give desired username and password"
+def Gquery(query):
+
+    """ 
+    print "Give desired username and password"
     user=raw_input("User:")
     password = getpass.getpass()
     """
+    SeqEntry sequences
+
     try:
         #Connecting
         con = mdb.connect('localhost', 'guest', 'password', 'genomedb')
         cur = con.cursor()
         #Query
-        print "Please insert desired query"
-        query=raw_input("Query:")
+        #print "Please insert desired query"
+        #query=raw_input("Query:"")
+
         cur.execute(query)
         rows=cur.fetchall()
         for row in rows:
+            if seqID in query:
+                for seq in sequences:
+                    seq.id = row
+            elif seqHash in query:
+                for seq in sequences:
+                    seq.hash = row
 
        except mdb.Error, e:
         print "Error %d: %s" % (e.args[0],e.args[1])
@@ -46,5 +57,7 @@ def Gquery():
     finally:
         if con:
             con.close()
+
+    return sequences
 
         
